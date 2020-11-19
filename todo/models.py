dcc4efef1e96aac03c7e595533ed34df831095c3
Phil Fiess <pfiess@protonmail.com>
2020-11-18 16:19:40 -0800

    registers and migrates task model to DB and registers with admin page

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
