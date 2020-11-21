0143c4ba7e2f83b43ed704d721f7a420ee787389
Phil Fiess <pfiess@protonmail.com>
2020-11-20 16:13:39 -0800

    adds url files

--- todolist/urls.py
+++ todolist/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.listTask, name="task_list"),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete_task/<str:pk>/', views.deleteTask, name="delete_task"),
 ]
