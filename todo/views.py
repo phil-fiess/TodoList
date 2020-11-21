772d801c6bffdfe2fc2c89a1e9c8b6a9273eb8a8
Phil Fiess <pfiess@protonmail.com>
2020-11-20 16:13:24 -0800

    adds views for pages

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
-# Create your views here.
+#Task list and form to create a new task
+def listTask(request):
+    queryset = task.objects.order_by('completed', 'due')
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)    
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    #renders data from DB
+    context = {
+        'tasks':queryset,
+        'form':form,
+    }
+    return render(request, 'list_task.html', context)
+
+#Update the task by primary key (pk)
+def updateTask(request, pk):
+    queryset = task.objects.get(id=pk)
+    form = UpdateForm(instance=queryset)
+    if request.method == 'POST':
+        form = UpdateForm(request.POST, instance=queryset)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    #render the updated data for display
+    context = {
+        'form':form
+    }
+    return render(request, 'update_task.html', context)
+
+#user can delete a task
+def deleteTask(request, pk):
+    queryset = task.objects.get(id=pk)
+    if request.method == 'POST':
+        queryset.delete()
+        return redirect('/')
+    context = {
+        'item':queryset
+    }
+    return render(request, 'delete_task.html', context)    
